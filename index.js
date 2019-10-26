038786ee83f7ad5aa782c75db1f06c6b01d83526
星伽　白雪 <hotogisirayuki9063@gmail.com>
2019-10-26 13:22:13 +0900

    First commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ cron.schedule('0 0 15 * * *',()=>{
 function jsonchecker(name){
   try{
     let txt = fs.readFileSync(name,'utf-8');
-    JSON.parse(txt.replace(/\/\/(.*?)\n|\n/g,'').replace(/\/\*(.*?)\*\//g,''))
+    JSON.parse(txt.replace(/\r/g,'\n').replace(/\/\/(.*?)\n| |\n|\/\*(.*?)\*\//g,''))
   }catch(e){
     console.log(name + '\n' + e.message)
     return "おっと jsonに不備があるようだ"
